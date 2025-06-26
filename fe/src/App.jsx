b1a68c1480849cd405839bb76ab233ae3c93fda4
rajyadav63 <vector36razz@gmail.com>
2025-06-26 20:19:11 +0530

    bg gradient added

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
 
 const App = () => {
   return (
-    <div>
-      Hello
+    <div className='px-5 sm:px-10 md:px-15 lg:px-32 min-h-screen bg-gradient-to-br from-[#04030e] via-[#1a0ea1] to-[#050511] text-white custom-stars'>
+      <Routes >
+        <Route path='/' element={<Home />} />
+        <Route path='/buy' element={<BuyCredit />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
     </div>
   )
 }
