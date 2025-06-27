c815e117b25055a5a2dee3013243567bc84d64f3
rajyadav63 <vector36razz@gmail.com>
2025-06-27 18:13:35 +0530

    navbar done

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import BuyCredit from './pages/BuyCredit'
-import Result from './pages/Result'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BuyCredit from "./pages/BuyCredit";
+import Result from "./pages/Result";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='py-8 px-5 sm:px-10 md:px-15 lg:px-32 min-h-screen bg-gradient-to-br from-[#04030e] via-[#1a0ea1] to-[#050511] text-white custom-stars'>
+    <div className="py-8 px-5 sm:px-10 md:px-15 lg:px-32 min-h-screen text-white"
+      style={{
+        backgroundImage: `radial-gradient(white 1px, transparent 1px), linear-gradient(to bottom right, #04030e, #1a0ea1, #050511)`,
+        backgroundSize: "30px 30px, cover",
+      }}>
 
       <Navbar />
 
-      <Routes >
-        <Route path='/' element={<Home />} />
-        <Route path='/buy' element={<BuyCredit />} />
-        <Route path='/result' element={<Result />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<BuyCredit />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
-    </div>
-  )
-}
 
-export default App
+    </div>
+  );
+};
+
+export default App;
--- fe/src/components/Navbar.jsx
+++ fe/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { Link, useNavigate } from 'react-router-dom'
+
+
 const Navbar = () => {
+    const { user } = useContext(AppContext)
+    const navigate = useNavigate()
+
     return (
         <div className='bg-slate-700 py-2 px-2 lg:px-8 rounded-lg z-[999] opacity-100 flex justify-between'>
 
@@ -9,13 +15,29 @@ const Navbar = () => {
                 <span className='font-bold lg:text-2xl'>Pictofy</span>
             </Link>
 
-
-            <div className='flex gap-3 lg:gap-12 items-center cursor-pointer'>
-                <p className='lg:text-2xl cursor-pointer'>Pricing</p>
-                <div className='flex items-center justify-center h-10 lg:h-12  bg-slate-950 hover:bg-slate-800 px-16  rounded-xl'>
-                    <p className='text-center font-semibold '>Login</p>
-                </div>
-            </div>
+            {
+                user ?
+                    <div className='flex items-center gap-4 md:gap-7 transition-all duration-700'>
+                        <div onClick={() => navigate('/buy')} className='flex items-center gap-3 bg-blue-300 px-2 py-2 lg:py-3 rounded-2xl hover:bg-blue-200 cursor-pointer transition-all duration-500'>
+                            <img src="/assets/credit_star.svg" alt="" />
+                            <p className='text-black'>Credits left: 0</p>
+                        </div>
+                        <p className='hidden sm:block'>Hii Razz</p>
+                        <div className='cursor-pointer relative group'>
+                            <img className='w-10 lg:w-12 group' src="/assets/profile_icon.png" />
+                            <div className='absolute top-12 right-0 hidden group-hover:block'>
+                                <button className='bg-blue-300 px-8 py-3 rounded-2xl text-black'>Logout</button>
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <div className='flex gap-3 lg:gap-12 items-center cursor-pointer'>
+                        <p onClick={() => navigate('/buy')} className='lg:text-2xl cursor-pointer'>Pricing</p>
+                        <div className='flex items-center justify-center h-10 lg:h-12  bg-slate-950 hover:bg-slate-800 px-16  rounded-xl'>
+                            <p className='text-center font-semibold '>Login</p>
+                        </div>
+                    </div>
+            }
         </div>
     )
 }
