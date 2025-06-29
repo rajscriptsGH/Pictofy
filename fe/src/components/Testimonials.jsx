48ff29239cac50fffae2fe7fe11e8053f6940abf
rajyadav63 <vector36razz@gmail.com>
2025-06-29 11:25:19 +0530

    transition added to testimonoals

--- fe/src/components/Testimonials.jsx
+++ fe/src/components/Testimonials.jsx
@@ -8,19 +8,19 @@ const Testimonials = () => {
                 <p className='font-light lg:text-2xl'>What our users are saying</p>
             </div>
             <div className='flex flex-col md:flex-row gap-5'>
-                <div className='text-center flex flex-col items-center gap-2 border px-10 py-3 rounded-xl hover:border-sky-500 max-w-[300px]'>
+                <div className='text-center flex flex-col items-center gap-2 border px-10 py-3 rounded-xl hover:border-sky-500 max-w-[300px] transition-all duration-300 hover:scale-105'>
                     <img className='h-[85px] rounded-full' src="assets/sample1.png" />
                     <p>Graphic Designer</p>
                     <p>⭐⭐⭐⭐⭐</p>
                     <p>I've been using imagify for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.</p>
                 </div>
-                <div className='text-center flex flex-col items-center gap-2 border px-10 py-3 rounded-xl hover:border-sky-500 max-w-[300px]'>
+                <div className='text-center flex flex-col items-center gap-2 border px-10 py-3 rounded-xl hover:border-sky-500 max-w-[300px] transition-all duration-300 hover:scale-105'>
                     <img src="assets/profile_img_2.png" />
                     <p>Content Creater</p>
                     <p>⭐⭐⭐⭐⭐</p>
                     <p>I've been using imagify for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.</p>
                 </div>
-                <div className='text-center flex flex-col items-center gap-2 border px-10 py-3 rounded-xl hover:border-sky-500 max-w-[300px]'>
+                <div className='text-center flex flex-col items-center gap-2 border px-10 py-3 rounded-xl hover:border-sky-500 max-w-[300px] transition-all duration-300 hover:scale-105'>
                     <img src="assets/profile_img_1.png" />
                     <p>Co-founder</p>
                     <p>⭐⭐⭐⭐⭐</p>
