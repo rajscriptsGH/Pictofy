605576ddc9a83a829e6949f04357d4614f5ec311
rajyadav63 <vector36razz@gmail.com>
2025-07-01 15:31:49 +0530

    Signup and signin switch added

--- fe/src/components/Login.jsx
+++ fe/src/components/Login.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [state, setState] = useState('Login')
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-9 backdrop-blur-sm bg-black/50 flex justify-center items-center'>
             <form className='bg-slate-200 text-black border px-8 sm:px-12 py-5 sm:py-10 rounded-2xl'>
                 <img className='relative top-0 left-72 cursor-pointer' src="/assets/cross_icon.svg" />
                 <div className='flex flex-col items-center gap-4 mb-5'>
-                    <h1 className='text-3xl font-medium'>Sign up</h1>
+                    <h1 className='text-3xl font-medium'>{state}</h1>
                     <p>Welcome back!  Please sign in to continue</p>
                 </div>
                 <div>
                     <div className='flex flex-col items-center gap-2'>
-                        <div className="relative w-full max-w-sm mb-4">
+                        {state !== 'Login' && <div className="relative w-full max-w-sm mb-4">
                             <img
                                 src="/assets/profile_icon.png"
                                 alt="profile"
@@ -23,7 +24,7 @@ const Login = () => {
                                 required
                                 className="pl-12 pr-4 py-3 w-full rounded-3xl outline-none border focus:ring-2 focus:ring-blue-500"
                             />
-                        </div>
+                        </div>}
 
                         <div className="relative w-full max-w-sm mb-4">
                             <img
@@ -56,10 +57,17 @@ const Login = () => {
                     </div>
                 </div>
                 <p className='mt-8  text-violet-700 cursor-pointer hover:underline'>Forgot Password?</p>
-                <button className='bg-blue-600 text-white px-24 py-2 mt-2 rounded-2xl outline-none hover:bg-blue-500'>Create account</button>
+                <button className='bg-blue-600 text-white px-32 py-2 mt-2 rounded-2xl outline-none hover:bg-blue-500'>{state === 'Login' ? "Login" : 'Create account'}</button>
 
-                <p className='mt-5 text-red-600'>Don't have an account? <span className='text-blue-600 underline cursor-pointer hover:no-underline'>Sign up</span></p>
-                <p className='mt-5'>Already have an account? <span className='text-blue-600 underline cursor-pointer hover:no-underline'>Login</span></p>
+                {state === 'Login'
+                    ?
+                    <p
+                        className='mt-5 text-red-600'>Don't have an account? <span className='text-blue-600 underline cursor-pointer hover:no-underline'
+                        onClick={()=> setState("Sign up")}
+                        >Sign up</span></p>
+                    :
+                    <p className='mt-5'>Already have an account? <span className='text-blue-600 underline cursor-pointer hover:no-underline' onClick={()=> setState("Login")}>Login</span></p>
+                }
             </form>
         </div>
     )
