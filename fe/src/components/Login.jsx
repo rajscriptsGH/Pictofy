12183829e654862a48e3b25094b6ad99d69f7b2f
rajyadav63 <vector36razz@gmail.com>
2025-07-01 12:36:10 +0530

    switch added

--- fe/src/components/Login.jsx
+++ fe/src/components/Login.jsx
@@ -4,9 +4,9 @@ const Login = () => {
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-9 backdrop-blur-sm bg-black/50 flex justify-center items-center'>
             <form className='bg-slate-200 text-black border px-8 sm:px-12 py-5 sm:py-10 rounded-2xl'>
-                <img className='relative top-0 left-72' src="/assets/cross_icon.svg" />
+                <img className='relative top-0 left-72 cursor-pointer' src="/assets/cross_icon.svg" />
                 <div className='flex flex-col items-center gap-4 mb-5'>
-                    <h1 className='text-3xl font-medium'>Login</h1>
+                    <h1 className='text-3xl font-medium'>Sign up</h1>
                     <p>Welcome back!  Please sign in to continue</p>
                 </div>
                 <div>
@@ -55,10 +55,11 @@ const Login = () => {
 
                     </div>
                 </div>
-                <p className='mt-8  text-violet-700'>Forgot Password?</p>
-                <button className='bg-blue-600 text-white px-32 py-2 mt-2 rounded-2xl hover:bg-blue-500'>Login</button>
+                <p className='mt-8  text-violet-700 cursor-pointer hover:underline'>Forgot Password?</p>
+                <button className='bg-blue-600 text-white px-24 py-2 mt-2 rounded-2xl outline-none hover:bg-blue-500'>Create account</button>
 
-                <p className='mt-5 text-red-600'>Don't have an account? <span className='text-blue-600 underline'>Sign up</span></p>
+                <p className='mt-5 text-red-600'>Don't have an account? <span className='text-blue-600 underline cursor-pointer hover:no-underline'>Sign up</span></p>
+                <p className='mt-5'>Already have an account? <span className='text-blue-600 underline cursor-pointer hover:no-underline'>Login</span></p>
             </form>
         </div>
     )
