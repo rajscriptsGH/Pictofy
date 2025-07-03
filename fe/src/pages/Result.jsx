8cad4492e9344b60ab84244c3c4dd74f7db31fcf
rajyadav63 <vector36razz@gmail.com>
2025-07-03 15:07:17 +0530

    motion added to result

--- fe/src/pages/Result.jsx
+++ fe/src/pages/Result.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 
 const Result = () => {
     const [image, setImage] = useState('/assets/sample1.png')
@@ -11,7 +12,12 @@ const Result = () => {
     }
 
     return (
-        <form onSubmit={onSubmitHandler} className='flex flex-col items-center gap-4'>
+        <motion.form onSubmit={onSubmitHandler}
+            initial={{ opacity: 0.2, y: 100 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            className='flex flex-col items-center gap-4'>
             <div className='mt-12'>
                 <div className='relative'>
                     <img className='h-[250px] rounded-lg' src={image} />
@@ -44,7 +50,7 @@ const Result = () => {
 
 
 
-        </form>
+        </motion.form>
     )
 }
 
