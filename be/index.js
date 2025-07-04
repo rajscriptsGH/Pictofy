7f80289dfab26270eeb48f3521573af416231b1c
rajyadav63 <vector36razz@gmail.com>
2025-07-04 09:57:05 +0530

    mongodb connected

--- be/index.js
+++ be/index.js
@@ -2,11 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
+import connectDB from './config/mongodb.js'
+
 const PORT = process.env.PORT || 3300
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+await connectDB()
 
 app.post('/', (req, res) => {
     res.send("api working")
