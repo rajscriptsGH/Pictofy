23b20c1fd6729fb8a4d043a71989c66bd1bfd0c2
rajyadav63 <vector36razz@gmail.com>
2025-07-05 10:43:48 +0530

    userController updated

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -53,7 +53,7 @@ const registerUser = async (req, res) => {
 };
 
 
-const userLogin = async (req, res) => {
+const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
 
