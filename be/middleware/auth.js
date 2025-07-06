a2b77147a38aa776be9be64b7a1a26c1c4eadce6
rajyadav63 <vector36razz@gmail.com>
2025-07-06 09:34:33 +0530

    comment added for better

--- be/middleware/auth.js
+++ be/middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const userAuth = async (req, res, next) => {
-    
+    //Step1: get token from headers
     const { token } = req.headers;
 
     if (!token) {
@@ -12,9 +12,13 @@ export const userAuth = async (req, res, next) => {
     }
 
     try {
+
+        //Step2: verify and decode it
         const decodeToken = jwt.verify(token, process.env.JWT_SECRET)
 
         if (decodeToken.id) {
+
+            //Step3: extract userID or username
             req.body.userID = decodeToken.id
         } else {
             return res.json({
