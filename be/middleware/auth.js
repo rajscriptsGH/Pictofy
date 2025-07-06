205b4d93f70ed99e814eb9c4104d6008a40047a4
rajyadav63 <vector36razz@gmail.com>
2025-07-06 09:29:02 +0530

    auth added

---
+++ be/middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+
+export const userAuth = async (req, res, next) => {
+    const { token } = req.headers;
+
+    if (!token) {
+        return res.json({
+            success: false,
+            msg: 'Not authorized login again'
+        });
+    }
+
+    try {
+        const decodeToken = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (decodeToken.id) {
+            req.body.userID = decodeToken.id
+        } else {
+            return res.json({
+                success: false,
+                msg: 'Not authorized login again'
+            })
+        }
+        next();
+
+    } catch (error) {
+        console.log(error);
+
+        return res.json({
+            success: false,
+            msg: error.msg
+        });
+    }
+}
